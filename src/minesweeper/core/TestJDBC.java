68bcf8448bd041bc07f77a966eeccc4bdd92d89f
PeterSrvatkaIT <psrvatka@gmail.com>
2022-08-03 12:33:10 +0200

    test

---
+++ src/minesweeper/core/TestJDBC.java
@@ -0,0 +1,24 @@
+package minesweeper.core;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class TestJDBC {
+
+    public static void main(String[] args) throws Exception {
+        try (var connection = DriverManager.getConnection("jdbc:postgresql://localhost/gamestudio", "postgres", "postgres");
+             var statement = connection.createStatement();
+             var rs = statement.executeQuery("SELECT game, username, points, played_on FROM score WHERE game='minesweeper' ORDER BY points DESC LIMIT 5")
+        ) {
+            while (rs.next()) {
+                System.out.printf("%s, %s, %d, %s \n", rs.getString(1), rs.getString(2), rs.getInt(3), rs.getTimestamp(4));
+            }
+            System.out.println("Pripojenie uspesne.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
